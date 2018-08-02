d8fc64544543569036332119f201371f0f89d32f
Bramhvanshi <himanshu-r.bramhvanshi@capgemini.com>
2018-08-02 20:01:56 +0530

    123

--- src/zedwallet/WalletConfig.h
+++ src/zedwallet/WalletConfig.h
@@ -10,10 +10,10 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string addressPrefix = "TRTL";
+    const std::string addressPrefix = "PCC";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
-    const std::string ticker = "TRTL";
+    const std::string ticker = "PCC";
 
     /* The filename to output the CSV to in save_csv */
     const std::string csvFilename = "transactions.csv";
@@ -23,7 +23,7 @@ namespace WalletConfig
     const std::string addressBookFilename = ".addressBook.json";
 
     /* The name of your deamon */
-    const std::string daemonName = "TurtleCoind";
+    const std::string daemonName = "POCCoind";
 
     /* The name to call this wallet */
     const std::string walletName = "zedwallet";
